17839117ad263ccdb3647352d32795372a1a4875
Flaviano Bezerra <flaviano202014@gmail.com>
2024-11-25 23:42:29 -0300

    feat: add new menu items and user info, improve navigation

--- src/components/dropDownMenu/index.tsx
+++ src/components/dropDownMenu/index.tsx
@@ -11,13 +11,21 @@ import RestrictedMode from '../../icons/restrictedMode';
 import PlaceIcon from '../../icons/place';
 import KeyboardShortcutsIcon from '../../icons/keyboardShortcuts';
 import NoImg from "../../assets/no-img.png";
-import { Link } from 'react-router-dom';
 import { UserContext } from '../../context/userContext';
+import LeaveIcon from '../../icons/leave';
+import GoogleAccountIcon from '../../icons/googleAccount';
+import ChangeAccountIcon from '../../icons/changeAccount';
+import YoutubeStudioIcon from '../../icons/youtubeStudio';
+import PurchasesAndSubscriptionIcon from '../../icons/purchasesAndSubscription';
+import NextButton from "../../assets/next-button.png";
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function DropDownMenu() {
-  const { login } = useContext(UserContext);
+  const { login, user } = useContext(UserContext);
   const [isOpen, setIsOpen] = useState(false);
   const DropDownRef = useRef<HTMLDivElement | null>(null);
+  const navigate = useNavigate();
 
   const handleClickOutside = (event: { target: any; }) => {
     if (DropDownRef.current && !DropDownRef.current.contains(event.target)) {
@@ -33,15 +41,20 @@ function DropDownMenu() {
   }, []);
 
   const menuItems = [
-    { icon: <YourDataIcon/>, label: 'Seus dados no YouTube' },
-    { icon: <AppearanceIcon/>, label: 'Tema do dispositivo' },
-    { icon: <LanguageIcon/>, label: 'Idioma: Português' },
-    { icon: <RestrictedMode/>, label: 'Modo restrito: desativado' },
-    { icon: <PlaceIcon/>, label: 'Local: Brasil' },
-    { icon: <KeyboardShortcutsIcon/>, label: 'Atalhos do teclado' },
-    { icon: <SettingsIcon/>, label: 'Configurações' },
-    { icon: <HelpIcon/>, label: 'Ajuda' },
-    { icon: <SendFeedbackIcon/>, label: 'Enviar feedback' },
+    { icon: <GoogleAccountIcon/>, link: '/google_account', label: 'Conta do Google' },
+    { icon: <ChangeAccountIcon/>, link: '/change_account', label: 'Mudar de conta', next: <img style={{ width: '25px' }} src={NextButton} alt='Next'/> },
+    { icon: <LeaveIcon/>, link: '/leave', label: 'Sair', divider: <div className='divider'></div> },
+    { icon: <YoutubeStudioIcon/>, link: '/youtube_studio', label: 'YouTube Studio' },
+    { icon: <PurchasesAndSubscriptionIcon/>, link: '/purchases_subscription', label: 'Compras e assinaturas', divider: <div className='divider'></div> },
+    { icon: <YourDataIcon/>, link: '/your_datas', label: 'Seus dados no YouTube' },
+    { icon: <AppearanceIcon/>, link: '/appearance', label: 'Tema do dispositivo', next: <img style={{ width: '25px' }} src={NextButton} alt='Next'/> },
+    { icon: <LanguageIcon/>, link: '/language', label: 'Idioma: Português', next: <img style={{ width: '25px' }} src={NextButton} alt='Next'/> },
+    { icon: <RestrictedMode/>, link: '/restricted_mode', label: 'Modo restrito: desativado', next: <img style={{ width: '25px' }} src={NextButton} alt='Next'/> },
+    { icon: <PlaceIcon/>, link: '/place', label: 'Local: Brasil', next: <img style={{ width: '25px' }} src={NextButton} alt='Next'/> },
+    { icon: <KeyboardShortcutsIcon/>, link: '/keyboard_shortcuts', label: 'Atalhos do teclado', divider: <div className='divider'></div> },
+    { icon: <SettingsIcon/>, link: '/settings', label: 'Configurações', divider: <div className='divider'></div> },
+    { icon: <HelpIcon/>, link: '/help', label: 'Ajuda' },
+    { icon: <SendFeedbackIcon/>, link: '/send_feedback', label: 'Enviar feedback' },
   ];
 
   return (
@@ -49,13 +62,31 @@ function DropDownMenu() {
       {login ? <img alt="" src={NoImg} style={{width: '100%', borderRadius: '50%'}} /> : <DropDownIcon/>}     
       {isOpen && (
         <DropDownContainer show={ isOpen }>
+          {login ? (
+            <>
+              <div style={{ height: '60px', display: 'flex', alignItems: 'center', padding: '12px' }}>
+                <img style={{ height: '40px', borderRadius: '50%' }} src={NoImg} alt='NOt imagem'/>
+                <div>
+                  <span>{user && user.name ? user.name : 'Erro ao buscar nome'}</span><br />
+                  <span>@{user && user.name ? user.name : 'Erro ao buscar nome'}</span><br />
+                  <Link to='/exemplo'>Acessar seu canal</Link>
+                </div>
+              </div>
+              <div className='divider'></div>            
+            </>
+            ) : null
+          }
           {menuItems.map((item, index) => (
-            <li key={index}>
-              <Link to="/exemplo">
-                {item.icon}
-                <span>{item.label}</span>
-              </Link>
-            </li>
+            <>
+              <li key={index} onClick={() => navigate(item.link)}>
+                <div style={{ display: 'flex' }}>
+                  {item.icon}
+                  <span>{item.label}</span>
+                </div>
+                <div>{item.next}</div>
+              </li>
+              <div>{item.divider}</div>            
+            </>
           ))}
         </DropDownContainer>
         
--- src/components/dropDownMenu/styles.ts
+++ src/components/dropDownMenu/styles.ts
@@ -17,7 +17,7 @@ export const Container = styled.div`
   
   
 export const DropDownContainer = styled.ul<{ show: boolean }>`
-  width: 240px;
+  width: 260px;
   position: absolute;
   top: 100%;  
   right: 0;
@@ -26,13 +26,19 @@ export const DropDownContainer = styled.ul<{ show: boolean }>`
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.8);
   padding: 5px;
   list-style: none;
-  border-radius: 20px;
+  border-radius: 10px;
   display: ${({ show }) => (show ? "block" : "none")};
+  overflow-y: auto;
+  height: calc(100vh - 100px);
 
   li {
-    border-radius: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-radius: 10px;
+    padding: 0 10px;
     cursor: pointer;
-    padding: 2px 15px;
+    height: 45px;
     box-sizing: border-box;
     font-size:14px;
     font-weight: 650;
@@ -51,5 +57,11 @@ export const DropDownContainer = styled.ul<{ show: boolean }>`
   
   span {
     margin-left: 10px;
+  }
+
+  .divider {
+    width: 100%;
+    border-bottom: 1px solid #bfbfbf;
+    margin: 12px 0;
   }
 `;

8a792dbdddc27465020a72c2898e3f9c98f1695d
Flaviano Bezerra <flaviano202014@gmail.com>
2024-12-11 23:38:42 -0300

    feat: add footer component

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,54 @@
+import { useContext } from "react";
+import LoginIcon from "../../icons/login";
+import RegistrationIcon from "../../icons/registration";
+import ShortsIcon from "../../icons/shorts";
+import StartIcon from "../../icons/start";
+import StudioIcon from "../../icons/studio";
+import { Container, FooterLogin, Img, Label, SubContainer } from "./styles";
+import { UserContext } from "../../context/userContext";
+import NoImg from "../../assets/no-img.png";
+import { useNavigate } from "react-router-dom";
+
+function Footer() {
+    const { login } = useContext(UserContext);
+    const navigate = useNavigate();
+
+    return (
+        <Container>
+            <SubContainer onClick={() => navigate('/')}>
+                <StartIcon />
+                <Label>Início</Label>
+            </SubContainer>
+            <SubContainer onClick={() => navigate('/shorts')}>
+                <ShortsIcon />
+                <Label>Shorts</Label>
+            </SubContainer>
+            {login ?
+                <SubContainer onClick={() => navigate('/studio')}>
+                    <StudioIcon />
+                </SubContainer>            
+            : null }
+            <SubContainer onClick={() => navigate('/registrations')}>
+                <RegistrationIcon />
+                <Label>Inscrições</Label>
+            </SubContainer>
+            <SubContainer>
+                {login ?
+                    <>
+                        <Img src={NoImg} alt="" />
+                        <Label>Você</Label>                    
+                    </>
+                :
+                    <>
+                        <FooterLogin onClick={() => navigate('/login')}>
+                            <LoginIcon />
+                            <Label>Login</Label>                
+                        </FooterLogin>
+                    </>              
+                }
+            </SubContainer>
+        </Container>
+    )
+}
+
+export default Footer;
---
+++ src/components/footer/styles.ts
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const Container = styled.footer`
+    width: 100%;
+    height: 55px;
+    background: #fff;
+    padding: 0 10px;
+    box-sizing: border-box;
+    display: none;
+    position: sticky;
+    bottom: 0;
+    z-index: 1;
+
+    @media(max-width: 768px){
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+    }
+`;
+
+export const Img = styled.img`
+    height: 25px;
+    width: 25px;
+    border-radius: 50%;
+`;
+
+export const SubContainer = styled.div`
+    height: auto;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    padding: 12px 0;
+    cursor: pointer;
+`;
+
+export const Label = styled.span`
+    font-size: 14px;
+    font-weight: 650;
+`;
+
+export const FooterLogin = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+`;

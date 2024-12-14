9c5e8e695c837bd29e269d9f0a04eb5138219743
Flaviano Bezerra <flaviano202014@gmail.com>
2024-12-13 23:32:47 -0300

    style: update layout and container styling for search page

--- src/pages/search/styles.ts
+++ src/pages/search/styles.ts
@@ -1,21 +1,22 @@
 import styled from "styled-components";
 
 export const SearchContainer = styled.div`
+  max-width: 1600px;
   width: 100%;
 `;
 
 export const SubContainer = styled.div`
    width: 100%;
    display: flex;
+   justify-content: center;
 `;
 
 export const Container = styled.div`
-  width: 100%;
-  max-width: 1600px;
+  width: 90%;
   display: flex;
   justify-content: center;
   flex-direction: column;
-  row-gap: 20px;
+  row-gap: 10px;
   padding: 10px 25px;
   box-sizing: border-box;
 `;
